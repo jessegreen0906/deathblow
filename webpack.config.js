e2e0f488547bb4959d97de2bbf3c05bec09b874f
Jesse Green <jessegreen0906@gmail.com>
2018-11-30 15:44:46 +1100

    "With reference to the SO thread that fixed my async issue"

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: './dist/dblo_client/dblo_client-app/index.js',
+	entry: ['babel-polyfill', './dist/dblo_client/dblo_client-app/index.js'],
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'build')
